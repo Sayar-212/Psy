419972e878cd7f2f070850b526bb41f8d809d4d9
HITK <hitkconfessions8@gmail.com>
2025-10-29 00:02:49 +0530

    Fix serverless function imports and add root requirements.txt

--- api/index.py
+++ api/index.py
@@ -1,4 +1,9 @@
+import sys
+import os
+
+# Add parent directory to path
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
 from backend.main import app
 
-# Vercel serverless function handler
-handler = app
+app = app
